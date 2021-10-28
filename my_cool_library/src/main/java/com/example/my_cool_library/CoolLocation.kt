4aa8c025146317dc74dfb83cfbf0485768661a97
Artyom Okun <rt.okun@gmail.com>
2021-10-28 17:03:07 +0300

    Added periodic updates

--- my_cool_library/src/main/java/com/example/my_cool_library/CoolLocation.kt
+++ my_cool_library/src/main/java/com/example/my_cool_library/CoolLocation.kt
@@ -1,11 +1,26 @@
 package com.example.my_cool_library
 
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+import kotlin.random.Random.Default.nextLong
+
 data class LatLng(val latitude: Long, val longitude: Long)
 
-class CoolLocation {
-    companion object{
-        fun getCurrentLocation(): LatLng{
-            return LatLng(latitude = 123, longitude = 456)
+object CoolLocation {
+
+    private var timer: Timer? = null
+
+    fun getLocationUpdates(every: Long = 5_000, callBack: (location: LatLng) -> Unit) {
+        timer = fixedRateTimer("timer", false, 0, every) {
+            callBack.invoke(LatLng(latitude = nextLong(), longitude = nextLong()))
         }
+    }
+
+    fun stopPeriodicUpdates() {
+        timer?.cancel()
+    }
+
+    fun getCurrentLocation(): LatLng {
+        return LatLng(latitude = 123, longitude = 456)
     }
 }
